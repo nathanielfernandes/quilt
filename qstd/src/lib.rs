da206d984e99f08235f55f2644732c93d6950295
nathanielfernandes <nathaniel.s.fernandes@gmail.com>
2023-09-10 12:04:28 -0400

    qstd time

--- qstd/src/lib.rs
+++ qstd/src/lib.rs
@@ -2,13 +2,15 @@ pub mod core;
 pub mod io;
 pub mod math;
 pub mod strings;
-mod testing;
+pub mod testing;
+pub mod time;
 
 pub use core::*;
 pub use io::*;
 pub use math::*;
 pub use strings::*;
 pub use testing::testing;
+pub use time::*;
 
 use interpreter::{builtins::VmData, vm::VM};
 
@@ -20,6 +22,8 @@ where
     vm.add_builtins(core);
     vm.add_builtins(math);
     vm.add_builtins(strings);
+    vm.add_builtins(testing);
+    vm.add_builtins(time);
 }
 
 #[inline]
---
+++ qstd/src/time.rs
@@ -0,0 +1,40 @@
+use common::error::Error;
+use interpreter::{builtins::*, generic_builtins, value::Value, vm::*};
+
+generic_builtins! {
+    [export=time]
+    [vm_options=options]
+
+    fn @now() {
+        match std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH) {
+                Ok(n) => n.as_secs_f64().into(),
+                Err(_) => Err(error("failed to get time"))?,
+            }
+    }
+
+    fn @date() {
+        let now = match std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH) {
+                Ok(n) => n,
+                Err(_) => return Err(error("failed to get time"))?,
+            };
+
+        let secs = now.as_secs();
+        let mins = secs / 60;
+        let hours = mins / 60;
+        let days = hours / 24;
+
+        let year = 1970 + (days / 365);
+        let month = (days % 365) / 30;
+        let day = (days % 365) % 30;
+
+        let hour = hours % 24;
+        let min = mins % 60;
+        let sec = secs % 60;
+
+        let date = format!("{}-{}-{} {}:{}:{}", year, month, day, hour, min, sec);
+
+        date.into()
+    }
+}
